2b2e3b337caec575f3addb43efc52bf6502dbf3b
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-03-20 13:13:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,69 +18,6 @@ require('./api/models/itemModel');
 require('./api/models/guideModel');
 require('./api/models/userModel');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(session({
-	secret: config.secret,
-	resave: true,
-	saveUninitialized: false
-}));
-
-
-// Add headers
-app.use(function (req, res, next) {
-	var allowedOrigins = ['http://www.lolhype.com', 'http://lolhype.com',  'http://localhost:4200', 'http://54.36.43.4'];
-	var origin = req.headers.origin;
-	if(allowedOrigins.indexOf(origin) > -1){
-		res.setHeader('Access-Control-Allow-Origin', origin);
-	}
-
-	res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	next();
-});
-
-
-var httpsServer = http.createServer(app);
-
-httpsServer.on('listening', function() {
-	console.log('is listening');
-})
-
-require('./api/routes/summonerRoutes')(app);
-require('./api/routes/spectatorRoutes')(app);
-require('./api/routes/staticDataRoutes')(app);
-require('./api/routes/guideRoutes')(app);
-require('./api/routes/userRoutes')(app);
-require('./api/routes/updateRoutes')(app);
-require('./api/routes/realmsRoutes')(app);
-
-httpsServer.listen(port);
-
-console.log('riot RESTful API server started on: ' + port);
-=======
-#!/user/bin/env node
-
-var http     = require('http'),
-	fs         = require('fs'),
-	express    = require('express'),
-	app        = express(),
-	port       = process.env.PORT || 3002,
-	mongoose   = require('mongoose'),
-	bodyParser = require('body-parser'),
-	session    = require('express-session'),
-	config     = require('./config.json');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://' + config.auth.user + ':' + config.auth.password + '@' + config.auth.ip + ':' + config.auth.port + '/' + config.auth.db)
-
-require('./api/models/staticDataChampionModel');
-require('./api/models/itemModel');
-require('./api/models/guideModel');
-require('./api/models/userModel');
-
 app.use(require('prerender-node').set('prerenderToken', 'qu3eZzSMnD54SQzjYLei'));
 
 app.use(express.static('public'));
@@ -127,4 +64,3 @@ require('./api/routes/realmsRoutes')(app);
 httpsServer.listen(port);
 
 console.log('riot RESTful API server started on: ' + port);
->>>>>>> 4fbdecd8a40918a7eaf96a0f78ac549c4691c01a
