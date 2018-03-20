d664ae0271ed880112bfa2d89f295c334303cd57
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-03-20 13:09:18 +0100

    1.0.0: Code review

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 var mongoose = require('mongoose'),
@@ -63,69 +62,3 @@ exports.get = function(req, resp) {
 	}
 
 };
-=======
-'use strict';
-
-var mongoose = require('mongoose'),
-	https = require('https'),
-	config = require('../../config.json'),
-	User = mongoose.model('User'),
-	mailController = require('../../components/mail/mailController');
-
-exports.post = function(req, resp) {
-	if (req.body.mail &&
-		req.body.pseudo &&
-		req.body.password) {
-
-		var userData = {
-			mail: req.body.mail,
-			pseudo: req.body.pseudo,
-			password: req.body.password,
-			new: true,
-		};
-
-		//use schema.create to insert data into the db
-		User.create(userData, function (err, user) {
-			if (err) {
-				resp.send(err);
-			} else {
-				resp.send(user);
-			}
-
-			mailController.send( 'registration', 'LoL Hypes Account', userData.mail, {
-				username: userData.pseudo
-			} );
-
-		});
-	}
-};
-
-exports.auth = function(req, resp) {
-	User.authenticate(req.body.mail, req.body.password, function (error, user) {
-      if (error || ! user ) {
-	      resp.send({
-					errors: true,
-					message: 'Wrong email or password.',
-				});
-      } else {
-        req.session.userId = user._id;
-        resp.send(user);
-      }
-    });
-}
-
-exports.get = function(req, resp) {
-	if ( req.params.id ) {
-		User.findOne({'_id': req.params.id}, function(err, users) {
-			if (err) {
-				resp.send(err);
-			}
-
-			resp.send(users);
-		});
-	} else {
-		resp.send([]);
-	}
-
-};
->>>>>>> 4fbdecd8a40918a7eaf96a0f78ac549c4691c01a
--- api/routes/guideRoutes.js
+++ api/routes/guideRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 var guideController = require('./../controllers/guideController');
 
@@ -21,27 +20,3 @@ module.exports = function(app) {
 	app.route('/vote')
 		.post(guideController.voteUp);
 };
-=======
-'use strict';
-var guideController = require('./../controllers/guideController');
-
-module.exports = function(app) {
-	app.route('/guide')
-		.put(guideController.put)
-		.post(guideController.post)
-	app.route('/guide/author/:authorId')
-	.get(guideController.getByAuthor)
-	app.route('/guide/:gameMode/:championId')
-		.get(guideController.get)
-	app.route('/guide/:gameMode/:championId/:state')
-		.get(guideController.getByGameModeChampionIdAndState)
-	app.route('/guide-favorite/:version/:gameMode/:championId/')
-		.get(guideController.getFavorite)
-	app.route('/guide/:id')
-		.get(guideController.get)
-	app.route('/guides')
-		.get(guideController.getAll);
-	app.route('/vote')
-		.post(guideController.voteUp);
-};
->>>>>>> 4fbdecd8a40918a7eaf96a0f78ac549c4691c01a
